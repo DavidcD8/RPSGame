fb0934e32fa48e155d88924c0d07ce3752fdcb2a
DavidcD8 <>
2023-05-08 15:34:00 +0000

    Add styles to main windows div, add icons to both players and NPC choice

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,7 +22,7 @@ function gameChoices() {
         createMenu.classList.add("scoreboard");
         createMenu.innerHTML = `<section id="scoreboard">
         <div id="numofwins">Wins <span id="score">0</span></div>
-        <div id="numofrounds"></div>
+        
 
     </section>
     <div id="main-window"></div>
@@ -51,42 +51,37 @@ function gameChoices() {
 
 function pickChoice() {
     let = picked = "";
-    let img = src = 'assets / images / stone.png';
+
 
     if (this.getAttribute('id') === "rock") {
-        document.getElementById('main-window').innerHTML = '<img id="img" width="100" height="100" src="../assets/images/stone.png">';
-
+        document.getElementById('main-window').innerHTML = `<i id="uno" class="fa-regular fa-hand-back-fist "></i>`;
         picked += "rock";
-        alert('rock');
+
     } else if (this.getAttribute('id') === "paper") {
-        document.getElementById('main-window').innerHTML = '<img id="img" width="100" height="100" src="../assets/images/paper-folding.gif">';
+        document.getElementById('main-window').innerHTML = `<i id="dos" class="fa-regular fa-hand"></i>`;
         picked += "paper";
-        alert('paper');
 
     } else if (this.getAttribute('id') === "scissors") {
-        document.getElementById('main-window').innerHTML = '<img id="img" width="100" height="100" src="../assets/images/scissors.png">';
-
+        document.getElementById('main-window').innerHTML = `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
         picked += "scissors";
-        alert('scissors');
+
     } else if (this.getAttribute('id') === "lizard") {
-        document.getElementById('main-window').innerHTML = '<img id="img" width="100" height="100" src="../assets/images/reptile.png">';
-
+        document.getElementById('main-window').innerHTML = `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
         picked += "lizard";
-        alert('lizard');
-    } else if (this.getAttribute('id') === "spock") {
-        document.getElementById('main-window').innerHTML = '<img id="img" width="100" height="100" src="../assets/images/ ">';
 
+    } else if (this.getAttribute('id') === "spock") {
+        document.getElementById('main-window').innerHTML = `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
         picked += "spock";
-        alert('spock');
+
     }
 
 }
 
 
-function showItem(picked) {
+function showItem() {
     let item = this.picked.innerText;
     return item;
-    document.getElementById("main-window").appendChild(item);
+
 }
 
 
@@ -115,10 +110,11 @@ function getRandomItem(arr) {
 }
 
 
-
 /**
  * This Function will take the value from the user's choice and comparet it to the random value and ouput the icon.
  */
+
+
 
 function compareChoices() {
     //calls random number funciton
@@ -127,46 +123,42 @@ function compareChoices() {
 
     //Comparison between rock vs all options
     if (picked === "rock" && result === "rock") {
-        document.getElementById('main-window').innerHTML += '<img  id="scissors" width="100" height="100" src="../assets/images/scissors.png">';
-        alert(`CPU picked ${result}"It's a tie"`);
+
+        document.getElementById('main-window').innerHTML += "It's a tie" + `<i id="uno" class="fa-regular fa-hand-back-fist "></i>`;
 
     }
 
 
     else if (picked === "rock" && result === "scissors") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuscissors" width="100" height="100" src="../assets/images/scissors.png">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
 
 
 
         wins = parseInt(document.getElementById("score").innerText);
         document.getElementById("score").innerText = ++wins;
         document.getElementById("wins").innerText = ++wins;
-
-
 
     }
 
     else if (picked === "rock" && result === "paper") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpupaper" width="100" height="100" src="../assets/images/paper-folding.gif">';
-        alert(`CPU picked ${result} You Lose!`);
-
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="dos" class="fa-regular fa-hand"></i>`;
 
     }
 
     else if (picked === "rock" && result === "lizard") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpureptile" width="100" height="100" src="../assets/images/reptile.png">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
+
 
 
         wins = parseInt(document.getElementById("score").innerText);
         document.getElementById("score").innerText = ++wins;
         document.getElementById("wins").innerText = ++wins;
+
+
     }
 
     else if (picked === "rock" && result === "spock") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuspock" width="100" height="100" src="../assets/images/p ">';
-        alert(`CPU picked ${result} You Lose!`);
+        document.getElementById('main-window').innerHTML += "You Lose!" `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
 
 
 
@@ -174,14 +166,14 @@ function compareChoices() {
 
     //Comparison between paper vs all options
     else if (picked === "paper" && result === "paper") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpupaper" width="100" height="100" src="../assets/images/paper-folding.gif">';
-        alert(`CPU picked ${result}"It's a tie"`);
+        document.getElementById('main-window').innerHTML += "It's a tie" + `<i id="dos" class="fa-regular fa-hand"></i>`;
+
 
     }
 
     else if (picked === "paper" && result === "rock") {
-        document.getElementById('main-window').innerHTML += '<img  id="stone" width="100" height="100" src="../assets/images/stone.png">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="uno" class="fa-regular fa-hand-back-fist"></i>`;
+
 
         wins = parseInt(document.getElementById("score").innerText);
         document.getElementById("score").innerText = ++wins;
@@ -191,20 +183,18 @@ function compareChoices() {
 
     }
     else if (picked === "paper" && result === "scissors") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuscissors" width="100" height="100" src="../assets/images/scissors.png">';
-        alert(`CPU picked ${result} You Lose!`);
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
+
 
     }
 
     else if (picked === "paper" && result === "lizard") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpureptile" width="100" height="100" src="../assets/images/reptile.png">';
-        alert(`CPU picked ${result} You Lose!`);
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
 
     }
 
     else if (picked === "paper" && result === "spock") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuspock" width="100" height="100" src="../assets/images/p ">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
 
         wins = parseInt(document.getElementById("score").innerText);
         document.getElementById("score").innerText = ++wins;
@@ -215,21 +205,20 @@ function compareChoices() {
 
     //Comparison between scissors vs all options
     else if (picked === "scissors" && result === "scissors") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuscissors" width="100" height="100" src="../assets/images/paper-scissors.png">';
-        alert(`CPU picked ${result}"It's a tie"`);
+        document.getElementById('main-window').innerHTML += "It's a tie" + `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
 
     }
 
     else if (picked === "scissors" && result === "rock") {
-        document.getElementById('main-window').innerHTML += '<img  id="stone" width="100" height="100" src="../assets/images/stone.png">';
-        alert(`CPU picked ${result} You Lose!`);
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="uno" class="fa-regular fa-hand-back-fist"></i>`;
+
 
 
 
     }
     else if (picked === "scissors" && result === "paper") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpupaper" width="100" height="100" src="../assets/images/paper-folding.gif">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="dos" class="fa-regular fa-hand"></i>`;
+
 
 
         wins = parseInt(document.getElementById("score").innerText);
@@ -239,8 +228,7 @@ function compareChoices() {
     }
 
     else if (picked === "scissors" && result === "lizard") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpureptile" width="100" height="100" src="../assets/images/reptile.png">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
 
 
         wins = parseInt(document.getElementById("score").innerText);
@@ -251,9 +239,7 @@ function compareChoices() {
     }
 
     else if (picked === "scissors" && result === "spock") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuspock" width="100" height="100" src="../assets/images/p ">';
-        alert(`CPU picked ${result} You Lose!`);
-
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
 
 
     }
@@ -261,21 +247,19 @@ function compareChoices() {
 
     //Comparison between lizard vs all options
     else if (picked === "lizard" && result === "lizard") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpureptile" width="100" height="100" src="../assets/images/reptile.png">';
-        alert(`CPU picked ${result}"It's a tie"`);
+        document.getElementById('main-window').innerHTML += "It's a tie" + `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
+
 
     }
 
     else if (picked === "lizard" && result === "rock") {
-        document.getElementById('main-window').innerHTML += '<img  id="stone" width="100" height="100" src="../assets/images/stone.png">';
-        alert(`CPU picked ${result} You Lose!`);
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="uno" class="fa-regular fa-hand-back-fist"></i>`;
 
 
 
     }
     else if (picked === "lizard" && result === "paper") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpupaper" width="100" height="100" src="../assets/images/paper-folding.gif">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="dos" class="fa-regular fa-hand"></i>`;
 
 
         wins = parseInt(document.getElementById("score").innerText);
@@ -285,17 +269,12 @@ function compareChoices() {
     }
 
     else if (picked === "lizard" && result === "scissor") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuscissors" width="100" height="100" src="../assets/images/reptile.png">';
-        alert(`CPU picked ${result} You Lose!`);
-
-
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
 
     }
 
     else if (picked === "lizard" && result === "spock") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuspock" width="100" height="100" src="../assets/images/p ">';
-        alert(`CPU picked ${result} You Win!`);
-
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
 
         wins = parseInt(document.getElementById("score").innerText);
         document.getElementById("score").innerText = ++wins;
@@ -308,14 +287,13 @@ function compareChoices() {
 
     //Comparison between lizard vs all options
     else if (picked === "spock" && result === "spock") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuspock" width="100" height="100" src="../assets/images/  ">';
-        alert(`CPU picked ${result}"It's a tie"`);
+        document.getElementById('main-window').innerHTML += "It's a tie" + `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
 
     }
 
     else if (picked === "spock" && result === "rock") {
-        document.getElementById('main-window').innerHTML += '<img  id="stone" width="100" height="100" src="../assets/images/stone.png">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" + `<i id="uno" class="fa-regular fa-hand-back-fist"></i>`;
+
 
 
         wins = parseInt(document.getElementById("score").innerText);
@@ -325,16 +303,13 @@ function compareChoices() {
 
     }
     else if (picked === "spock" && result === "paper") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpupaper" width="100" height="100" src="../assets/images/paper-folding.gif">';
-        alert(`CPU picked ${result} You Lose!`);
-
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="dos" class="fa-regular fa-hand"></i>`;
 
 
     }
 
     else if (picked === "spock" && result === "scissor") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpuscissors" width="100" height="100" src="../assets/images/scissors.png">';
-        alert(`CPU picked ${result} You Win!`);
+        document.getElementById('main-window').innerHTML += "You Win!" `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
 
 
         wins = parseInt(document.getElementById("score").innerText);
@@ -345,13 +320,10 @@ function compareChoices() {
     }
 
     else if (picked === "spock" && result === "lizard") {
-        document.getElementById('main-window').innerHTML += '<img  id="cpureptile" width="100" height="100" src="../assets/images/reptile.png ">';
-        alert(`CPU picked ${result} You Lose!`);
-
+        document.getElementById('main-window').innerHTML += "You Lose!" + `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
 
 
     }
 
-
-
+    document.getElementById('main - window').innerHTML = "";
 }
