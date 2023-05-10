bd3205cdfc5d3fb30f03949eb2ab8a0c4a3c619a
DavidcD8 <>
2023-05-10 11:18:04 +0000

    Add color to main window, Add border to windows. Add position to signs and timer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,88 +10,99 @@ myClick.addEventListener('click', gameChoices);
 
 
 /**
- * This function will remove the Start menu
+ * This function will remove the Start menu and create the game interface
 */
 function gameChoices() {
+    let titlebar = document.getElementById('mainscreentitle');
+    titlebar.remove();
     let start = document.getElementById('start');
     start.remove();
 
     if (myClick) {
 
+        //The game interface will be created if when start button is pressed
         let createMenu = document.createElement("section");
+        //will add class to section 
         createMenu.classList.add("scoreboard");
-        createMenu.innerHTML = `<section id="scoreboard">
-        <div id="numofwins">Wins <span id="score">0</span></div>
+        createMenu.innerHTML = ` <section id="scoreboard">
+        <div id="numofwins">Wins: <span id="score">0</span></div>
         
-
+        
     </section>
+    
+    
     <div id="main-window"></div>
+    
+    
     <div id="choice-panel">
+    
+    
     <button type="submit" class="choices" id="rock"><i class="fa-regular fa-hand-back-fist"></i></button>
+    
+    
     <button type="submit" class="choices" id="paper"><i class="fa-regular fa-hand"></i></button>
+    
+    
     <button type="submit" class="choices" id="scissors"><i class="fa-regular fa-hand-scissors"></i></button>
+    
+    
     <button type="submit" class="choices" id="lizard"><i class="fa-regular fa-hand-lizard"></i></button>
+    
+    
     <button type="submit" class="choices" id="spock"><i class="fa-regular fa-hand-spock"></i></button>
-    <button type="submit" onclick="playGame().document.getElementById('main-window').innerHTML.remove();" id="playbtn">Play</button>
+    
+    
     </div>`;
 
         body.appendChild(createMenu);
     }
+
+    //will get the choice from player and call the pickChocie function
     let cClick = document.getElementsByClassName('choices');
     for (let i = 0; i < cClick.length; i++) {
         cClick[i].addEventListener('click', pickChoice);
     }
 
-    document.getElementById("numofwins").appendChild(wins);
+    document.getElementById("numofwins").appendChild();
 
 }
 
 
-
-
+/**
+ * This function will add the player choice to the main window div and display it on screen
+*/
 function pickChoice() {
     let = picked = "";
-
 
     if (this.getAttribute('id') === "rock") {
         document.getElementById('main-window').innerHTML = `<i id="uno" class="fa-regular fa-hand-back-fist "></i>`;
         picked += "rock";
+        compareChoices();
 
     } else if (this.getAttribute('id') === "paper") {
         document.getElementById('main-window').innerHTML = `<i id="dos" class="fa-regular fa-hand"></i>`;
         picked += "paper";
+        compareChoices();
 
     } else if (this.getAttribute('id') === "scissors") {
         document.getElementById('main-window').innerHTML = `<i id="tres" class="fa-regular fa-hand-scissors"></i>`;
         picked += "scissors";
+        compareChoices();
 
     } else if (this.getAttribute('id') === "lizard") {
         document.getElementById('main-window').innerHTML = `<i id="cuatro" class="fa-regular fa-hand-lizard"></i>`;
         picked += "lizard";
+        compareChoices();
 
     } else if (this.getAttribute('id') === "spock") {
         document.getElementById('main-window').innerHTML = `<i id="cinco" class="fa-regular fa-hand-spock"></i>`;
         picked += "spock";
-
+        compareChoices();
     }
 
 }
 
 
-function showItem() {
-    let item = this.picked.innerText;
-    return item;
-
-}
-
-
-
-
-
-function playGame() {
-
-    compareChoices();
-}
 
 
 
@@ -111,22 +122,25 @@ function getRandomItem(arr) {
 
 
 /**
- * This Function will take the value from the user's choice and comparet it to the random value and ouput the icon.
+ * This Function will take the value from the user's choice and compare it to the random value and  display the choice on screen
  */
-
-
 
 function compareChoices() {
     //calls random number funciton
     let result = getRandomItem(cpuArray);
 
+    setTimeout(function () {
+        document.getElementById("main-window").innerHTML = "";
+    }, 3000);
 
     //Comparison between rock vs all options
     if (picked === "rock" && result === "rock") {
 
         document.getElementById('main-window').innerHTML += "It's a tie" + `<i id="uno" class="fa-regular fa-hand-back-fist "></i>`;
 
+
     }
+
 
 
     else if (picked === "rock" && result === "scissors") {
@@ -325,5 +339,7 @@ function compareChoices() {
 
     }
 
-    document.getElementById('main - window').innerHTML = "";
+
 }
+
+
